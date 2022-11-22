043cd1117f0b672f3407a9731996b3bf096cab95
Martin Due Frandsen <mdf@energinet.dk>
2022-11-22 13:13:55 +0100

    Add tests for validation failure

--- EnerginetDemo/Tests/Unit/Application/SampleMessageServiceTests.cs
+++ EnerginetDemo/Tests/Unit/Application/SampleMessageServiceTests.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 using AutoFixture;
 using EnerginetDemo.Application;
 using EnerginetDemo.Application.Converters;
@@ -8,6 +10,8 @@ using EnerginetDemo.Application.Validators;
 using EnerginetDemo.Domain.Database;
 using EnerginetDemo.Domain.Input;
 using EnerginetDemo.Infrastructure;
+using FluentAssertions;
+using FluentValidation.Results;
 using Moq;
 using Xunit;
 
@@ -32,7 +36,7 @@ public class SampleMessageServiceTests : TestBase<SampleMessageService>
     private Mock<ISampleMessageRepository> SampleMessageRepositoryMock { get; }
 
     [Fact]
-    public async Task Should_HandleMessage_When_MessageIsValid()
+    public async Task Should_HandleMessageAccordingly_When_MessageIsValid()
     {
         // Arrange
         var stream = Fixture.Create<Stream>();
@@ -45,5 +49,29 @@ public class SampleMessageServiceTests : TestBase<SampleMessageService>
         SampleMessageValidatorMock.Verify(x => x.ValidateAsync(It.IsAny<SampleMessage>(), It.IsAny<CancellationToken>()), Times.Once);
         SampleMessageConverterMock.Verify(x => x.Convert(It.IsAny<SampleMessage>()), Times.Once);
         SampleMessageRepositoryMock.Verify(x => x.Add(It.IsAny<SampleMessageDb>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Should_ThrowException_When_ValidationFails()
+    {
+        // Arrange
+        var stream = Fixture.Create<Stream>();
+        var expectedErrorMessage = "Text should be 10 chars or longer";
+        var validationResult = new List<ValidationFailure>
+        {
+            new("Text", expectedErrorMessage)
+        };
+
+        SampleMessageValidatorMock.Setup(x => x.ValidateAsync(It.IsAny<SampleMessage>(), It.IsAny<CancellationToken>()))
+            .Returns(() => Task.FromResult(new ValidationResult(validationResult)));
+
+        // Act
+        var actual = async () => await Sut.HandleIncomingSampleMessage(stream);
+
+        // Assert
+        await actual.Should().ThrowAsync<XmlSchemaValidationException>().WithMessage(expectedErrorMessage);
+
+        SampleMessageDeserializerMock.Verify(x => x.DeserializeMessageAsync(It.IsAny<Stream>()), Times.Once);
+        SampleMessageValidatorMock.Verify(x => x.ValidateAsync(It.IsAny<SampleMessage>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
