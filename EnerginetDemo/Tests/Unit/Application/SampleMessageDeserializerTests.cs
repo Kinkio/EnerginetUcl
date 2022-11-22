9d29bd1b5fdc8b3ca32a50ae2b7902647197e23f
Martin Due Frandsen <mdf@energinet.dk>
2022-11-22 11:30:26 +0100

    Add tests for deserializer

---
+++ EnerginetDemo/Tests/Unit/Application/SampleMessageDeserializerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using EnerginetDemo.Application;
+using EnerginetDemo.Domain.Input;
+using FluentAssertions;
+using Xunit;
+
+namespace EnerginetDemo.Tests.Unit.Application;
+
+public class SampleMessageDeserializerTests : TestBase<SampleMessageDeserializer>
+{
+    public SampleMessageDeserializerTests()
+    {
+        TestDataFolder = Path.Combine(Directory.GetCurrentDirectory(), "Tests/TestData");
+    }
+
+    private string TestDataFolder { get; }
+
+    [Fact]
+    public async Task Should_DeserializeToSampleMessage_When_ValidXmlGiven()
+    {
+        // Arrange
+        var testDataFile = Path.Combine(TestDataFolder, "SampleMessage.xml");
+        await using var fileStream = File.OpenRead(testDataFile);
+
+        // Act
+        var actual = await Sut.DeserializeMessageAsync(fileStream);
+
+        // Assert
+        actual.Text.Should().Be("A sample message.");
+    }
+}
